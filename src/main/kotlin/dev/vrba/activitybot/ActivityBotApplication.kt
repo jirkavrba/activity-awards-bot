ec3d6b548780a642bb8fba60fdb26a680ec0ad65
Jiří Vrba <jiri@vrba.dev>
2022-03-24 15:38:48 +0100

    Add discord bot configuration wrapper class

--- src/main/kotlin/dev/vrba/activitybot/ActivityBotApplication.kt
+++ src/main/kotlin/dev/vrba/activitybot/ActivityBotApplication.kt
@@ -1,9 +1,11 @@
 package dev.vrba.activitybot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = ["dev.vrba.activitybot.configuration"])
 class ActivityBotApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/dev/vrba/activitybot/configuration/DiscordBotConfiguration.kt
@@ -0,0 +1,11 @@
+package dev.vrba.activitybot.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "discord")
+class DiscordBotConfiguration(
+    val token: String,
+    val developmentGuildId: Long
+)

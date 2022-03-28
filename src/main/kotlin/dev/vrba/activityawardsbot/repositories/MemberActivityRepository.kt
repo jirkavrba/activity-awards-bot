f9786baf4437ac9fa3c570fe5d36af9d1c2b77b7
Jiří Vrba <jiri@vrba.dev>
2022-03-28 09:28:27 +0200

    Formatting

--- src/main/kotlin/dev/vrba/activityawardsbot/repositories/MemberActivityRepository.kt
+++ src/main/kotlin/dev/vrba/activityawardsbot/repositories/MemberActivityRepository.kt
@@ -3,6 +3,7 @@ package dev.vrba.activityawardsbot.repositories
 import dev.vrba.activityawardsbot.dtos.MemberActivitySummary
 import dev.vrba.activityawardsbot.entities.MemberActivity
 import org.springframework.data.domain.Sort
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
@@ -16,11 +17,11 @@ interface MemberActivityRepository : CrudRepository<MemberActivity, UUID> {
 
     @Query(
         """
-       select new dev.vrba.activityawardsbot.dtos.MemberActivitySummary(m.member, sum(m.messages) as messages) 
+        select new dev.vrba.activityawardsbot.dtos.MemberActivitySummary(m.member, sum(m.messages) as messages) 
         from MemberActivity m 
         where m.guild = ?1 and m.date > ?2 
         group by m.member
-    """
+        """
     )
     fun getActivitySummaries(guild: Long, date: LocalDate, sort: Sort): List<MemberActivitySummary>
 
